1486e86cb5ea0f385b7f73ce700935e6d8a18b5f
yaxing.chen <chenyaxing@rt-thread.com>
2021-02-01 11:51:41 +0800

    first commit

---
+++ project_0/rtconfig_preinc.h
@@ -0,0 +1,14 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define AT32F403AVGT7
+#define HAVE_CCONFIG_H
+#define RT_USING_NEWLIB
+#define USE_STDPERIPH_DRIVER
+#define __RTTHREAD__
+
+#endif /*RTCONFIG_PREINC_H__*/
